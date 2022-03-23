aa89600aa0e8f64d77ecef6e2a177cbf7f7b6294
HJX <73078933+DEVTOOL877@users.noreply.github.com>
2022-03-23 20:10:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my crmusic you using www. you will Forwarding to crmusic.tk!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Thanks <code>!</code>
         </p>
       </main>
 
